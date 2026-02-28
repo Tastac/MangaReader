c0e33032d6ebc05b49c4f684ed51b2df5f577f54
Tastac <keyganap@gmail.com>
2026-02-27 20:52:34 -0800

    Initial Next.js project scaffold with auth

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+export default function NotFound() {
+  return (
+    <html>
+        <head>
+
+        </head>
+        <body>
+            <main style={{ padding: 24 }}>
+                
+            </main>
+        </body>
+  </html>
+  );
+}
